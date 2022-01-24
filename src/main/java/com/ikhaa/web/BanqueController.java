a4ec2c0c7a6ecb7024f878bd5cb6fb5d7da0f0e1
abdelkarimm <abdelkarim01.ma@gmail.com>
2022-01-24 23:02:49 +0100

    Update BanqueController.java

--- src/main/java/com/ikhaa/web/BanqueController.java
+++ src/main/java/com/ikhaa/web/BanqueController.java
@@ -74,19 +74,19 @@ public class BanqueController {
 	}
 	
 	@RequestMapping("/users/export/excel")
-    public void exportToExcel(HttpServletResponse response) throws IOException {
-        response.setContentType("application/octet-stream");
-        DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
-        String currentDateTime = dateFormatter.format(new Date());
-         
-        String headerKey = "Content-Disposition";
-        String headerValue = "attachment; filename=users_" + currentDateTime + ".xlsx";
-        response.setHeader(headerKey, headerValue);
-         
-        List<Operation> listUsers = banqueMetier.listAll();
-         
-        UserExcelExporter excelExporter = new UserExcelExporter(listUsers);
-         
-        excelExporter.export(response);    
-    }  
+        public void exportToExcel(HttpServletResponse response) throws IOException {
+		response.setContentType("application/octet-stream");
+		DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
+		String currentDateTime = dateFormatter.format(new Date());
+
+		String headerKey = "Content-Disposition";
+		String headerValue = "attachment; filename=users_" + currentDateTime + ".xlsx";
+		response.setHeader(headerKey, headerValue);
+
+		List<Operation> listUsers = banqueMetier.listAll();
+
+		UserExcelExporter excelExporter = new UserExcelExporter(listUsers);
+
+		excelExporter.export(response);    
+	    }  
 }
