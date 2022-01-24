b1abeedf7ed9cf78177dd5c6d14e0940f4cb6f6d
abdelkarimm <abdelkarim01.ma@gmail.com>
2022-01-24 23:22:00 +0100

    Update Operation.java

--- src/main/java/com/ikhaa/entities/Operation.java
+++ src/main/java/com/ikhaa/entities/Operation.java
@@ -2,7 +2,6 @@ package com.ikhaa.entities;
 
 import java.io.Serializable;
 import java.util.Date;
-
 import javax.persistence.DiscriminatorColumn;
 import javax.persistence.DiscriminatorType;
 import javax.persistence.Entity;
@@ -16,8 +15,8 @@ import javax.persistence.ManyToOne;
 @Entity
 @Inheritance(strategy=InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name="TYPE_OP",discriminatorType=DiscriminatorType.STRING,length=1)
-
 public abstract class Operation implements Serializable{
+	
 	@Id @GeneratedValue
 	private Long numero;
 	private Date dateOperation;
@@ -25,9 +24,9 @@ public abstract class Operation implements Serializable{
 	@ManyToOne
 	@JoinColumn(name="CODE_CPTE")
 	private Compte compte;
+	
 	public Operation() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 	public Operation(Date dateOperation, double montant, Compte compte) {
 		super();
