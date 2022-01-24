7449c7ba88fc8fb14011435ad870a44c272abd88
abdelkarimm <abdelkarim01.ma@gmail.com>
2022-01-24 23:19:28 +0100

    Update OperationRepository.java

--- src/main/java/com/ikhaa/dao/OperationRepository.java
+++ src/main/java/com/ikhaa/dao/OperationRepository.java
@@ -8,8 +8,8 @@ import org.springframework.data.repository.query.Param;
 
 import com.ikhaa.entities.Operation;
 
-public interface OperationRepository 
-extends JpaRepository<Operation , Long>{
+public interface OperationRepository extends JpaRepository<Operation , Long>{
+	
 	@Query("select o from Operation o where o.compte.codeCompte=:x order by o.dateOperation desc")
 	public Page<Operation> listOperation(@Param("x")String codeCpte,Pageable pageable);
 }
