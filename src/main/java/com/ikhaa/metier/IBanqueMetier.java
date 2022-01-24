307512df60273a5947b6dd2be0bc1878da0acff2
abdelkarimm <abdelkarim01.ma@gmail.com>
2022-01-24 23:17:40 +0100

    Update IBanqueMetier.java

--- src/main/java/com/ikhaa/metier/IBanqueMetier.java
+++ src/main/java/com/ikhaa/metier/IBanqueMetier.java
@@ -1,18 +1,20 @@
 package com.ikhaa.metier;
 
 import java.util.List;
-
 import org.springframework.data.domain.Page;
-
 import com.ikhaa.entities.Compte;
 import com.ikhaa.entities.Operation;
 
 public interface IBanqueMetier {
 
 	public Compte consulterCompte(String codeCpte);
+	
 	public void verser(String codeCpte, double montant);
+	
 	public void retirer(String codeCpte, double montant);
+	
 	public Page<Operation> listOperation(String codeCpte,int page,int size);
+	
 	public List<Operation> listAll();
 
 }
