1ac9f6e4e30f7f56d61f33c026d7ad11d5c96509
abdelkarimm <abdelkarim01.ma@gmail.com>
2022-01-24 23:15:17 +0100

    Update Compte.java

--- src/main/java/com/ikhaa/entities/Compte.java
+++ src/main/java/com/ikhaa/entities/Compte.java
@@ -13,6 +13,7 @@ import javax.persistence.InheritanceType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+
 @Entity
 public  class  Compte implements Serializable {
 	@Id
